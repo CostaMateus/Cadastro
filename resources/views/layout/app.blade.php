b38987eae299948395248b1880d6122259575ae6
mateus.costa <mateus.costa@criarenet.com>
2019-10-31 12:36:25 -0300

    Model Produtos e Categorias

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,17 +12,17 @@
             }
         </style>
     </head>
-<body>
-    <div class="container">
-        @component('componente_navbar', [ "current" => $current ])
-        @endcomponent
-        <main role="main">
-            @hasSection('body')
-                @yield('body')
-            @endif
-        </main>
-    </div>
-    
-    <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
-</body>
+    <body>
+        <div class="container">
+            @component('componente_navbar', [ "current" => $current ])
+            @endcomponent
+            <main role="main">
+                @hasSection('body')
+                    @yield('body')
+                @endif
+            </main>
+        </div>
+
+        <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+    </body>
 </html>
